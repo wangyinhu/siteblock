a95819fa873ede616a33fc2db0b994452a35b5d6
yale <yinhuwang@gmail.com>
2020-11-24 13:32:23 +0800

    add hide mode, and context menu

--- js/background.js
+++ js/background.js
@@ -1,172 +1,237 @@
 blocked_urls = [];
+hidden_urls = [];
 
 function removeUrlAll(arr, url) {
-  var i = 0;
-  while (i < arr.length) {
-    if (arr[i].startsWith(url)) {
-      arr.splice(i, 1);
-    } else {
-      ++i;
+    var i = 0;
+    while (i < arr.length) {
+        if (arr[i].startsWith(url)) {
+            arr.splice(i, 1);
+        } else {
+            ++i;
+        }
     }
-  }
-  return arr;
+    return arr;
 }
 
 function strip_url(url) {
-  var domainstart = url.indexOf("/") + 2;
-  var pathstart = url.substring(domainstart).indexOf("/");
-  if (pathstart === -1){
-    return url;
-  } else{
-    return url.substring(0, domainstart + pathstart + 1);
-  }
-}
-
-
-function check_blocked(url){
-  console.log('Checking if url is blocked, utl=:');
-  console.log(url);
-  console.log("url strip:");
-  console.log(strip_url(url));
-  console.log("blocked_urls:");
-  console.log(blocked_urls);
-  if(url.startsWith("chrome-extension://") && url.includes("blockedSite.html")){
-    return 'blockedSite';
-  }
-
-  if(url.startsWith("chrome-extension://") || url.startsWith("chrome://")){
-    return 'chrome';
-  }
-
-  for(const i in blocked_urls){
-    if(url.startsWith(blocked_urls[i])){
-      return 'blocked';
-    }
-  }
-  return 'pass';
-}
-
-function save_list(action, url) {
-  chrome.storage.local.get({
-    sync: 'sync',
-    server: "https://www.example.com/list/"
-  }, function (items){
-    if(items.sync === 'sync'){
-      chrome.storage.sync.set({'blocked': blocked_urls}, function () {
-        console.log("urls saved into sync.");
-      })
-    } else if (items.sync === 'local'){
-      chrome.storage.local.set({'blocked': blocked_urls}, function () {
-        console.log("urls saved into local.");
-      })
-    } else if (items.sync === 'server'){
-      const server = items.server;
-      const request = new XMLHttpRequest();
-      request.open("POST", server, true);
-      request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-      request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-      request.onreadystatechange = function() {
-
-        if (this.readyState === 4 ){
-          if( this.status === 200) {
-            var myArr = JSON.parse(this.responseText);
-            if(myArr.code === 'done'){
-              console.log("urls saved into server.");
-            }
-          }else{
-            console.log("urls saving into server failed.");
-          }
-        }
-      };
-      request.send(JSON.stringify({'action': action, 'urls': [url]}));
+    var domainstart = url.indexOf("/") + 2;
+    var pathstart = url.substring(domainstart).indexOf("/");
+    if (pathstart === -1) {
+        return url;
     } else {
-      console.log("unknown 'sync' value.");
+        return url.substring(0, domainstart + pathstart + 1);
     }
-  });
+}
+
+function check_blocked(url) {
+    console.log('Checking if url is blocked, utl=:');
+    console.log(url);
+    console.log("url strip:");
+    console.log(strip_url(url));
+    console.log("blocked_urls:");
+    console.log(blocked_urls);
+    console.log("hidden_urls:");
+    console.log(hidden_urls);
+    if (url.startsWith("chrome-extension://") && url.includes("blockedSite.html")) {
+        return 'blockedSite';
+    }
+
+    if (url.startsWith("chrome-extension://") || url.startsWith("chrome://")) {
+        return 'chrome';
+    }
+
+    for (const i in blocked_urls) {
+        if (url.startsWith(blocked_urls[i])) {
+            return 'blocked';
+        }
+    }
+
+    for (const i in hidden_urls) {
+        if (url.startsWith(hidden_urls[i])) {
+            return 'hidden';
+        }
+    }
+    return 'pass';
+}
+
+function save_list(action, type, url) {
+    if(type === 'hide'){
+        if(action === 'add'){
+            hidden_urls.push(strip_url(url));
+        } else if(action === 'remove'){
+            hidden_urls = removeUrlAll(hidden_urls, strip_url(url));
+        }
+    }else if(type === 'block'){
+        if(action === 'add'){
+            blocked_urls.push(strip_url(url));
+        } else if(action === 'remove'){
+            blocked_urls = removeUrlAll(blocked_urls, strip_url(url));
+        }
+    }
+    chrome.storage.local.get({
+        sync: 'sync',
+        server: "https://www.example.com/list/"
+    }, function (items) {
+        if (items.sync === 'sync') {
+            if (type === 'block') {
+                chrome.storage.sync.set({'blocked': blocked_urls}, function () {
+                    console.log("urls saved into sync.");
+                })
+            } else if (type === 'hide') {
+                chrome.storage.sync.set({'hidden': hidden_urls}, function () {
+                    console.log("urls saved into sync.");
+                })
+            }
+        } else if (items.sync === 'local') {
+            if (type === 'block') {
+                chrome.storage.local.set({'blocked': blocked_urls}, function () {
+                    console.log("urls saved into local.");
+                })
+            } else if (type === 'hide') {
+                chrome.storage.local.set({'hidden': hidden_urls}, function () {
+                    console.log("urls saved into local.");
+                })
+            }
+        } else if (items.sync === 'server') {
+            const server = items.server;
+            const request = new XMLHttpRequest();
+            request.open("POST", server, true);
+            request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+            request.onreadystatechange = function () {
+
+                if (this.readyState === 4) {
+                    if (this.status === 200) {
+                        var myArr = JSON.parse(this.responseText);
+                        if (myArr.code === 'done') {
+                            console.log("urls saved into server.");
+                        }
+                    } else {
+                        console.log("urls saving into server failed.");
+                    }
+                }
+            };
+            request.send(JSON.stringify({'action': action, 'type': type, 'urls': [url]}));
+        } else {
+            console.log("unknown 'sync' value.");
+        }
+    });
 }
 
 function load_list() {
-  chrome.storage.local.get({
-    sync: 'sync',
-    server: "https://example.com"
-  }, function (items){
-    if(items.sync === 'sync'){
-      chrome.storage.sync.get('blocked', function(items) {
-        blocked_urls = items['blocked'];
-        if(blocked_urls === undefined){
-          blocked_urls = [];
+    chrome.storage.local.get({
+        sync: 'sync',
+        server: "https://example.com"
+    }, function (items) {
+        if (items.sync === 'sync') {
+            chrome.storage.sync.get('blocked', function (items) {
+                blocked_urls = items['blocked'];
+                if (blocked_urls === undefined) {
+                    blocked_urls = [];
+                }
+                console.log("urls loaded from sync.");
+            });
+            chrome.storage.sync.get('hidden', function (items) {
+                hidden_urls = items['hidden'];
+                if (hidden_urls === undefined) {
+                    hidden_urls = [];
+                }
+                console.log("urls loaded from sync.");
+            });
+        } else if (items.sync === 'local') {
+            chrome.storage.local.get('blocked', function (items) {
+                blocked_urls = items['blocked'];
+                if (blocked_urls === undefined) {
+                    blocked_urls = [];
+                }
+                console.log("urls loaded from local.");
+            });
+            chrome.storage.local.get('hidden', function (items) {
+                hidden_urls = items['hidden'];
+                if (hidden_urls === undefined) {
+                    hidden_urls = [];
+                }
+                console.log("urls loaded from local.");
+            });
+        } else if (items.sync === 'server') {
+            const server = items.server;
+            const request = new XMLHttpRequest();
+            request.open("POST", server, true);
+            request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+            request.onreadystatechange = function () {
+                if (this.readyState === 4) {
+                    if (this.status === 200) {
+                        var myArr = JSON.parse(this.responseText);
+                        if (myArr.code === 'done') {
+                            blocked_urls = myArr.blocked_urls;
+                            hidden_urls = myArr.hidden_urls;
+                            console.log("urls loaded from server:");
+                            console.log("blocked_urls");
+                            console.log(blocked_urls);
+                            console.log("hidden_urls");
+                            console.log(hidden_urls);
+                        }
+                    } else {
+                        console.log("this.readyState");
+                        console.log(this.readyState);
+                        console.log(this.responseText);
+                        console.log("urls loading from server failed.");
+                    }
+                }
+            };
+            request.send(JSON.stringify({'action': 'query', 'urls': []}));
+        } else {
+            console.log("unknown 'sync' value.");
         }
-        console.log("urls loaded from sync.");
-      });
-    } else if(items.sync === 'local'){
-      chrome.storage.local.get('blocked', function(items) {
-        blocked_urls = items['blocked'];
-        if(blocked_urls === undefined){
-          blocked_urls = [];
-        }
-        console.log("urls loaded from local.");
-      });
-    } else if(items.sync === 'server'){
-      const server = items.server;
-      const request = new XMLHttpRequest();
-      request.open("POST", server, true);
-      request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-      request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-      request.onreadystatechange = function() {
-        if (this.readyState === 4 ) {
-          if (this.status === 200) {
-            var myArr = JSON.parse(this.responseText);
-            if (myArr.code === 'done') {
-              blocked_urls = myArr.urls;
-              console.log("urls loaded from server:");
-              console.log("blocked_urls");
-              console.log(blocked_urls);
-            }
-          } else {
-            console.log("this.readyState");
-            console.log(this.readyState);
-            console.log(this.responseText);
-            console.log("urls loading from server failed.");
-          }
-        }
-      };
-      request.send(JSON.stringify({'action': 'query', 'urls': []}));
-    } else {
-      console.log("unknown 'sync' value.");
-    }
-  });
+    });
 }
 
-function add_blocked(url){
-  if(url.startsWith("chrome-extension://")){
-    return;
-  }
-  blocked_urls.push(strip_url(url));
-  save_list('add', strip_url(url));
+function add_blocked(url) {
+    save_list('add', 'block', strip_url(url));
 }
 
-function rm_blocked(url){
-  if(url.startsWith("chrome-extension://")){
-    return;
-  }
-  blocked_urls = removeUrlAll(blocked_urls, strip_url(url));
-  save_list('remove', strip_url(url));
+function add_hidden(url) {
+    save_list('add', 'hide', strip_url(url));
+}
+
+function rm_blocked(url) {
+    save_list('remove', 'block', strip_url(url));
 }
 
 function requestChecker(request) {
-  if (request && request.url && request.type === "main_frame") {
-    if (check_blocked(request.url) === 'blocked') {
-      var redirectUrl = chrome.extension.getURL(
-          "blockedSite.html?blocked=" + request.url);
-      return { redirectUrl: redirectUrl };
+    if (request && request.url && request.type === "main_frame") {
+        if (check_blocked(request.url) === 'blocked') {
+            var redirectUrl = chrome.extension.getURL(
+                "blockedSite.html?blocked=" + request.url);
+            return {redirectUrl: redirectUrl};
+        } else if (check_blocked(request.url) === 'hidden' && !chrome.extension.inIncognitoContext) {
+            // chrome.tabs.remove(request.tabId);
+            chrome.windows.create({url: request.url , incognito: true});
+            // return {cancel: true};
+            return {redirectUrl: 'javascript:void(0)'};
+        }
     }
-  }
 }
 
-function onload () {
-  load_list();
+function onload() {
+    load_list();
+}
+
+function context_cb(object){
+    if (!chrome.extension.inIncognitoContext){
+        add_hidden(object.linkUrl);
+        chrome.windows.create({url: object.linkUrl , incognito: true});
+    }
 }
 
 chrome.webRequest.onBeforeRequest.addListener(requestChecker, {urls: ["*://*/*"]}, ["blocking"]);
 chrome.runtime.onStartup.addListener(onload);
 chrome.runtime.onInstalled.addListener(onload);
+
+chrome.contextMenus.create({
+    'title':'always in incognito',
+    'contexts':['link'],
+    'id':'contexEnter',
+    'onclick':context_cb
+});
--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,8 @@
+/**
+ * get blocked url from url="chrome-extension://hbfjieomldekdngendoebbebofojgfap/blockedSite.html?blocked=www.example.com"
+ * @param url
+ * @returns {string}
+ */
 function get_blocked_url(url) {
   url = url.substring(url.indexOf("?"));
   return url.substring(url.indexOf("=") + 1);
@@ -6,18 +11,33 @@ function get_blocked_url(url) {
 function sitePass() {
   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let url = get_blocked_url(tabs[0].url);
-    chrome.extension.getBackgroundPage().rm_blocked(url);
-    chrome.tabs.update({url: url});
-    window.close();
+    if (!url.startsWith("chrome-extension://")) {
+      chrome.extension.getBackgroundPage().rm_blocked(url);
+      chrome.tabs.update({url: url});   // goto passed site
+      window.close();   //close popup
+    }
   });
 }
 
 function siteBlock() {
   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let url = tabs[0].url;
-    chrome.extension.getBackgroundPage().add_blocked(url);
-    chrome.tabs.update({url: url});
-    window.close();
+    if (!url.startsWith("chrome-extension://")) {
+      chrome.extension.getBackgroundPage().add_blocked(url);
+      chrome.tabs.update({url: url});     //reload to block page
+      window.close();     //close popup
+    }
+  });
+}
+
+function siteHide() {
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let url = tabs[0].url;
+    if (!url.startsWith("chrome-extension://")) {
+      chrome.extension.getBackgroundPage().add_hidden(url);
+      chrome.tabs.update({url: url});     //reload to block page
+      window.close();     //close popup
+    }
   });
 }
 
@@ -32,20 +52,24 @@ function options() {
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
   let url = tabs[0].url;
   var button = $("#blacklistButton")
+  var hideButton = $("#hideButton")
   var opButton = $("#optionsButton")
 
   if (chrome.extension.getBackgroundPage().check_blocked(url) === 'pass') {
-    button.disabled = false;
     button.click(siteBlock);
     button.text("block site");
-  } else if (chrome.extension.getBackgroundPage().check_blocked(url) === 'blockedSite'){
-    button.disabled = false;
-    button.click(sitePass);
-    var disp_url = chrome.extension.getBackgroundPage().strip_url(get_blocked_url(url))
-    button.text("Remove " + disp_url + " from the blocked list");
-  } else {
-    button.click(options);
-    button.text("hey!");
+    hideButton.click(siteHide);
+    hideButton.text("inco")
+  } else{
+    hideButton.hide();
+    if (chrome.extension.getBackgroundPage().check_blocked(url) === 'blockedSite'){
+      button.click(sitePass);
+      var disp_url = chrome.extension.getBackgroundPage().strip_url(get_blocked_url(url))
+      button.text("Remove " + disp_url + " from the blocked list");
+    } else {
+      button.hide();
+      hideButton.hide();
+    }
   }
   opButton.click(options);
 });
